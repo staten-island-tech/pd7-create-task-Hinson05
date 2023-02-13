f76763cf850cfa1ca821bad688a868fac58929b6
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2023-02-13 14:01:36 -0500

    a

--- hincreate/main.js
+++ hincreate/main.js
@@ -27,6 +27,5 @@ insert();
 DOMSelectors.input.insertAdjacentHTML(
   "afterbegin",
   `<p class= "yes"> Name: ${x}  
-  Input: ${}
   <img id= "image" src= 'https://flagsapi.com/${flag}/flat/64.png'></p>`
 );
